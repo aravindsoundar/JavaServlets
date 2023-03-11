d1414f2a363943b1b701395042256ad983d253d1
unknown <aravindsoundar2k2@gmail.com>
2023-03-11 17:29:34 +0530

    added a demo server application

---
+++ DemoApp/src/main/java/com/Aravind/AddServlet.java
@@ -0,0 +1,22 @@
+package com.Aravind;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet  
+{
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int k = i+j;
+		
+		PrintWriter pw = res.getWriter();
+		pw.println("result is "+k);
+	}
+}
